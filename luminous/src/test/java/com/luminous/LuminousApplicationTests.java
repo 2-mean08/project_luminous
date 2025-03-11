4748179d6802f162580f2cdeb2cfb4dcf28df2bc
leejm <leejm010813@gmail.com>
2025-03-12 00:18:45 +0900

    250312 개발 환경 구성

---
+++ luminous/src/test/java/com/luminous/LuminousApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luminous;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LuminousApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
