3ac7ec9245cd8f9f71eea983a7138cff69c46bf0
leejm <leejm@218.39.245.139>
2025-04-04 01:12:25 +0900

    250404 개발환경 구축 및 깃헙 연동

---
+++ lumiuns/src/main/java/com/luminus/LumiunsApplication.java
@@ -0,0 +1,13 @@
+package com.luminus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LumiunsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LumiunsApplication.class, args);
+	}
+
+}
---
+++ lumiuns/src/test/java/com/luminus/LumiunsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luminus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LumiunsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
